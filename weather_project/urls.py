3926aef0fdc2707105d7ffdb2816b5ec3ee09850
Veda Abdhija <vedaabdhija.0709@gmail.com>
2026-09-15 13:25:29 +0530

    Add files via upload

---
+++ weather_project/urls.py
@@ -0,0 +1,14 @@
+"""
+URL configuration for weather_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('forecast.urls')), # Include URLs from our forecast app
+]
+
